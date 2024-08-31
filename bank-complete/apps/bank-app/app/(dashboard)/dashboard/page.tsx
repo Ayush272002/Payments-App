875dcd10443f91b869680a8c41b96c3f6ee60ae8
Ayush Acharjya <ayushacharjya@gmail.com>
2024-08-31 11:45:57 +0100

    complete v1

--- bank-complete/apps/bank-app/app/(dashboard)/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function () {
-  return <div className="text-2xl">Dashboard</div>;
-}
--- bank-complete/packages/ui/src/Heading.tsx
+++
@@ -1,9 +0,0 @@
-interface HeadingProps {
-  label: string;
-}
-
-export const Heading = ({ label }: HeadingProps) => {
-  return <div className="font-bold text-4xl pt-6">{label}</div>;
-};
-
-export default Heading;
